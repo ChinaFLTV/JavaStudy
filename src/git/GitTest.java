1cb6c4d1e7db299f6caedc7c464550e8b4def3f4
ChinaFLTV <109414112+ChinaFLTV@users.noreply.github.com>
2022-07-17 16:12:11 +0800

    Pull Test
    emmmm.....

--- src/git/GitTest.java
+++ src/git/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
     public void main(String[] args) throws Exception {
 
         System.out.println("Hope that I can push successfully!");
+        System.out.println("JuJingyi loves Dage!");
 
     }
 
